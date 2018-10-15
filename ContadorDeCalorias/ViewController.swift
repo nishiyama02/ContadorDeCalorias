b264a49d8f9062d9052a378d83dead4910d930a2
Matheus Nishiyama Alves <mnmalves@voegol.com.br>
2018-10-15 17:01:09 -0300

    ViewController e Test implementados

---
+++ ContadorDeCalorias/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  ContadorDeCalorias
+//
+//  Created by Matheus Nishi on 15/10/18.
+//  Copyright © 2018 nishi.Matheus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    let counter = ContadorDeCaloria()
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    
+}
+
+class ContadorDeCaloria {
+    
+    let maxCaloriasPorDia: Int = 2000
+    
+    let caloriasPorFeijoada: Int = 500
+    
+    private var totalCaloriasConsumidasHoje: Int = 0
+    
+    func add(caloriasToAdd: Int) {
+        totalCaloriasConsumidasHoje += caloriasToAdd
+    }
+    
+    var feijoadasAindaPossoComerHoje: Int {
+        return (maxCaloriasPorDia - totalCaloriasConsumidasHoje) / caloriasPorFeijoada
+    }
+}
---
+++ ContadorDeCaloriasTests/ContadorDeCaloriasTests.swift
@@ -0,0 +1,42 @@
+//
+//  ContadorDeCaloriasTests.swift
+//  ContadorDeCaloriasTests
+//
+//  Created by Matheus Nishi on 15/10/18.
+//  Copyright © 2018 nishi.Matheus. All rights reserved.
+//
+
+import XCTest
+@testable import ContadorDeCalorias
+
+class ContadorDeCaloriasTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+    func testFeijoadaPossoComerAindaHoje() {
+        let counter = ContadorDeCaloria()
+        
+        XCTAssert(counter.feijoadasAindaPossoComerHoje <= 4, "ERRO 1")
+        XCTAssert(counter.feijoadasAindaPossoComerHoje >= 0, "ERRO 2")
+        
+    }
+
+}
